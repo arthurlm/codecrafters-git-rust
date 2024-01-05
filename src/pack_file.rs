8beddbded50620e88171b711b2e0e159e3767178
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2024-01-05 18:32:50 +0100

    Rebuild object from patch

--- src/pack_file.rs
+++ src/pack_file.rs
@@ -3,7 +3,7 @@ use std::{
     path::Path,
 };
 
-use bytes::{Buf, Bytes};
+use bytes::Buf;
 use flate2::bufread::ZlibDecoder;
 
 use crate::{
@@ -47,7 +47,7 @@ where
     // Read object.
     for _object_id in 0..object_count {
         // Read object header.
-        let (obj_type, obj_len) = read_object_size(&mut reader)?;
+        let (obj_type, obj_len) = read_object_pack_header(&mut reader)?;
 
         // Decode object content based on its type.
         match obj_type {
@@ -92,8 +92,12 @@ where
                 let mut decompress = ZlibDecoder::new(&mut reader);
                 decompress.read_to_end(&mut patch_data)?;
 
-                // Apply patch and save data
-                apply_patch(&base_data, &patch_data);
+                // Apply patch to rebuild git object.
+                let object = apply_patch(&base_data, &patch_data)?;
+
+                // Write object to dst.
+                let (hash_code, obj_content) = object.to_bytes_vec()?;
+                write_compressed_at(hash_code, &obj_content, dst.clone())?;
             }
             OBJ_OFS_DELTA | OBJ_TAG => {
                 unimplemented!("Unimplemented object type decoding: {obj_type}")
@@ -110,7 +114,7 @@ where
     Ok(())
 }
 
-pub fn read_object_size<R: io::Read>(reader: &mut R) -> io::Result<(u8, usize)> {
+pub fn read_object_pack_header<R: io::Read>(reader: &mut R) -> io::Result<(u8, usize)> {
     // Read first byte.
     let mut buf = [0_u8; 1];
     reader.read_exact(&mut buf)?;
@@ -127,14 +131,34 @@ pub fn read_object_size<R: io::Read>(reader: &mut R) -> io::Result<(u8, usize)> 
         reader.read_exact(&mut buf)?;
 
         // Read payload.
-        msb = buf[0] & 0b1000_0000;
-
-        // Big endian.
+        msb = buf[0] >> 7;
         value |= ((buf[0] & 0b0111_1111) as usize) << offset;
         offset += 7;
     }
 
     Ok((obj_type, value))
+}
+
+pub fn read_var_int(data: &[u8]) -> Result<(&[u8], usize), GitError> {
+    let mut msb = 1;
+    let mut index = 0;
+    let mut value = 0_usize;
+
+    while msb != 0 {
+        // Read next byte.
+        let Some(byte) = data.get(index) else {
+            return Err(GitError::invalid_content(
+                "Missing next byte in var int read",
+            ));
+        };
+
+        // Read payload.
+        msb = byte >> 7;
+        value |= ((byte & 0b0111_1111) as usize) << (index * 7);
+        index += 1;
+    }
+
+    Ok((&data[index..], value))
 }
 
 fn unpack_git_object<R, P>(reader: &mut R, header: GitObjectHeader, dst: P) -> Result<(), GitError>
@@ -157,8 +181,123 @@ where
     Ok(())
 }
 
-fn apply_patch(base: &[u8], patch: &[u8]) {
-    let patch = Bytes::copy_from_slice(patch);
-    let base = Bytes::copy_from_slice(base);
-    println!("patch_data: {patch:?}, base_data: {base:?}");
+fn apply_patch(base: &[u8], patch: &[u8]) -> Result<GitObject, GitError> {
+    // Split header and payload from base data.
+    let Some(base_header_offset) = base.iter().position(|x| *x == 0) else {
+        return Err(GitError::invalid_content("base miss header"));
+    };
+
+    let (base_header, base_payload) = base.split_at(base_header_offset + 1);
+    let base_header = GitObjectHeader::read(&mut base_header.reader())?;
+
+    // Read header and init output object.
+    let (patch, source_len) = read_var_int(patch)?;
+    assert_eq!(
+        source_len,
+        base_payload.len(),
+        "Base len is different from information stored in patch {base:?}"
+    );
+
+    let (mut patch, output_len) = read_var_int(patch)?;
+    let mut output = Vec::with_capacity(output_len);
+
+    // Loop over instruction and rebuild output object.
+    while !patch.is_empty() {
+        let (next_patch, instr) = DeltaInstructionType::from_bytes(patch);
+        patch = next_patch;
+
+        match instr {
+            DeltaInstructionType::Copy { offset, size } => {
+                output.extend_from_slice(&base_payload[offset..offset + size]);
+            }
+            DeltaInstructionType::Insert { size } => {
+                let (next_payload, next_patch) = patch.split_at(size);
+                patch = next_patch;
+                output.extend_from_slice(next_payload);
+            }
+            DeltaInstructionType::Reserved => {}
+        }
+    }
+
+    // Build git object from rebuild content + header
+    let object = GitObject::read_with_header(
+        &mut output.reader(),
+        GitObjectHeader {
+            len: output_len,
+            r#type: base_header.r#type,
+        },
+    )?;
+
+    Ok(object)
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum DeltaInstructionType {
+    Copy { offset: usize, size: usize },
+    Insert { size: usize },
+    Reserved,
+}
+
+impl DeltaInstructionType {
+    pub fn from_bytes(input: &[u8]) -> (&[u8], Self) {
+        let instr = input[0];
+
+        // If MSB is set: then it is a copy.
+        if instr >> 7 == 1 {
+            let mut bytes_read = 1;
+
+            // Read next 4 optional bytes to get offset to copy from.
+            let mut offset = 0;
+            if instr & 0x01 != 0 {
+                offset |= input[bytes_read] as usize;
+                bytes_read += 1;
+            }
+            if instr & 0x02 != 0 {
+                offset |= (input[bytes_read] as usize) << 8;
+                bytes_read += 1;
+            }
+            if instr & 0x04 != 0 {
+                offset |= (input[bytes_read] as usize) << 16;
+                bytes_read += 1;
+            }
+            if instr & 0x08 != 0 {
+                offset |= (input[bytes_read] as usize) << 24;
+                bytes_read += 1;
+            }
+
+            // Read next 3 optional bytes to get object size.
+            let mut size = 0;
+            if instr & 0x10 != 0 {
+                size |= input[bytes_read] as usize;
+                bytes_read += 1;
+            }
+            if instr & 0x20 != 0 {
+                size |= (input[bytes_read] as usize) << 8;
+                bytes_read += 1;
+            }
+            if instr & 0x40 != 0 {
+                size |= (input[bytes_read] as usize) << 16;
+                bytes_read += 1;
+            }
+
+            // If size is 0: then set size to a special value.
+            // See: https://github.com/git/git/blob/795ea8776befc95ea2becd8020c7a284677b4161/Documentation/gitformat-pack.txt#L156
+            if size == 0 {
+                size = 0x10000;
+            }
+
+            (&input[bytes_read..], Self::Copy { offset, size })
+        } else {
+            // Otherwise it is an insert instruction
+            let size = (instr & 0b0111_1111) as usize;
+
+            // Handle case of 0 size instruction.
+            // See: https://github.com/git/git/blob/795ea8776befc95ea2becd8020c7a284677b4161/Documentation/gitformat-pack.txt#L169
+            if size == 0 {
+                (&input[1..], Self::Reserved)
+            } else {
+                (&input[1..], Self::Insert { size })
+            }
+        }
+    }
 }
--- tests/test_pack_file.rs
+++ tests/test_pack_file.rs
@@ -2,7 +2,7 @@ use std::env;
 
 use bytes::{Buf, Bytes};
 use git_starter_rust::{
-    pack_file::{read_object_size, unpack_into},
+    pack_file::{read_object_pack_header, unpack_into, DeltaInstructionType},
     GitError,
 };
 
@@ -42,10 +42,44 @@ fn test_valid_parse() {
 #[test]
 fn test_read_object_size() {
     fn read(data: &'static [u8]) -> (u8, usize) {
-        read_object_size(&mut Bytes::from_static(data).reader()).unwrap()
+        read_object_pack_header(&mut Bytes::from_static(data).reader()).unwrap()
     }
 
     assert_eq!(read(&[0b0000_0000]), (0, 0));
     assert_eq!(read(&[0b0001_0100]), (1, 4));
     assert_eq!(read(&[0b1101_0001, 0b1001_0000, 0b0010_0000]), (5, 65793));
+}
+
+#[test]
+fn test_decode_delta_instr() {
+    fn check(input: &[u8], expected: DeltaInstructionType) {
+        let (rem, out) = DeltaInstructionType::from_bytes(input);
+        assert_eq!(out, expected);
+        assert_eq!(rem.len(), 0);
+    }
+
+    // Reserved
+    check(&[0x00], DeltaInstructionType::Reserved);
+
+    // Copy
+    check(
+        &[0b1011_0000, 0b1101_0001, 0b000_00001],
+        DeltaInstructionType::Copy {
+            offset: 0,
+            size: 465,
+        },
+    );
+    check(
+        &[0b1010_1100, 0b1001_0000, 0b1111_0001, 0b0000_1110],
+        DeltaInstructionType::Copy {
+            offset: (0b1001_0000_usize << 16) | (0b1111_0001_usize << 24),
+            size: 0b0000_1110_usize << 8,
+        },
+    );
+
+    // Insert
+    check(
+        &[0b0111_0010],
+        DeltaInstructionType::Insert { size: 0b0111_0010 },
+    );
 }
